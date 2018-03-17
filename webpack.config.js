596ebf98063176be4b3c1c753ebd8a441e3e43a4
0dysseas <kaziolas.od@gmail.com>
2018-03-17 23:44:28 +0200

    Added scss files-not working for webpack >=4

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
- 
  const path = require('path');
  const CleanWebpackPlugin = require('clean-webpack-plugin');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const ExtractTextPlugin = require('extract-text-webpack-plugin');
+ const webpack = require('webpack');
+ const extractPlugin = new ExtractTextPlugin({ filename: './assets/css/app.css' })
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
@@ -12,7 +15,7 @@ module.exports = {
 		filename: './assets/js/[name].bundle.js'
 	},
 	devServer: {
-		commentBase: path.resolve(__dirname, 'dist/assets/media'),
+		contentBase: path.resolve(__dirname, './dist/assets/media'),
 		stats: 'errors-only',
 		open: true,
 		port: 12000,
@@ -30,11 +33,45 @@ module.exports = {
 					presets: ['env']
 				}
 			}
-		  } 
+		 },
+
+		 { 
+		 	test: /\.html$/,
+		 	use: ['html-loader']
+		 },
+
+		 {
+		 	  test: /\.scss$/,
+  			  include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
+  			  use: extractPlugin.extract({
+    		    use: [
+    		    {
+    		    	loader: 'css-loader', 
+    		    	options: {
+    		    		sourceMap: true
+    		    	}
+    		    },
+
+    		    {
+    		   		loader: 'sass-loader',
+    		   		options: {
+    		   			sourceMap: true
+    		   		}
+    		    }
+    		    ],    		   
+    			fallback: 'style-loader'
+  			  })	
+		 }	 	
 		]
-	}
+	},
+
 	devtool: 'inline-source-map',
+
 	plugins : [
-		new CleanWebpackPlugin(['dist'])
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			template: 'index.html'
+		}),
+		extractPlugin //broken for >= webpack 4.0
 	]
 }
